b212aeee07390665d29715a50db56bb4104d8af3
Abhijeet <122082856+abhijeetpoonia@users.noreply.github.com>
2025-01-29 12:07:22 +0530

    Create outputs.tf

---
+++ modules/asg/outputs.tf
@@ -0,0 +1,13 @@
+output "asg_id" {
+  description = "The ID of the Auto Scaling Group"
+  value       = aws_autoscaling_group.this.id
+}
+
+output "asg_arn" {
+  description = "The ARN of the Auto Scaling Group"
+  value       = aws_autoscaling_group.this.arn
+}
+
+output "asg_name" {
+  description = "The name of the Auto Scaling Group"
+  value       = aws_autoscaling_group.this.name
