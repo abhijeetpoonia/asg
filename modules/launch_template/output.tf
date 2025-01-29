05c0411d39c359fd5b4fc6ea703906de38f87cde
Abhijeet <122082856+abhijeetpoonia@users.noreply.github.com>
2025-01-29 12:09:40 +0530

    Create output.tf

---
+++ modules/launch_template/output.tf
@@ -0,0 +1,13 @@
+output "launch_template_id" {
+  description = "The ID of the launch template"
+  value       = aws_launch_template.this.id
+}
+
+output "launch_template_latest_version" {
+  description = "The latest version of the launch template"
+  value       = aws_launch_template.this.latest_version
+}
+
+output "launch_template_name" {
+  description = "The name of the launch template"
+  value       = aws_launch_template.this.name
