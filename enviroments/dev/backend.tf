ce253e700a50f9e91aa100415d4ca641d95c616d
Abhijeet <122082856+abhijeetpoonia@users.noreply.github.com>
2025-01-30 10:50:29 +0530

    Update backend.tf

--- enviroments/dev/backend.tf
+++ enviroments/dev/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "asgbucket121"
-    key    = "asg/dev/terraform.tfstate"
+    key    = "asg/prod/terraform.tfstate"
     region = "ap-south-1"
     encrypt = true
   }
